23bfdff376148a2d0a398a88a7d789c7c382111b
Herziox <herziox@gmail.com>
2021-08-18 23:40:33 -0500

    Programa basico

--- app/src/main/java/com/example/standbyme/MainActivity.java
+++ app/src/main/java/com/example/standbyme/MainActivity.java
@@ -4,11 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int var1 = 10, var2= 8, var3= 5, prom=0;
+        prom = (var1+var2+var3)/3;
+
+        if (prom >= 7){
+            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Reprobado", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
+
+
 }
