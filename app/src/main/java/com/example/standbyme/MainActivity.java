b237a97af607db97a7f18b5b6530782b7c685763
Herziox <herziox@gmail.com>
2021-08-24 17:38:06 -0500

    Desarrollo de interfaz de Persona Encargada

--- app/src/main/java/com/example/standbyme/MainActivity.java
+++ app/src/main/java/com/example/standbyme/MainActivity.java
@@ -3,26 +3,35 @@ package com.example.standbyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText et1, et2;
+    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //La clase R es una clase de comunicacion entre la parte logica y grafica de la app
+        et1 = (EditText) findViewById(R.id.txt_num1);
+        et2 = (EditText) findViewById(R.id.txt_num2);
+        tv1 = (TextView) findViewById(R.id.txt_resultado);
+    }
 
-        int var1 = 10, var2= 8, var3= 5, prom=0;
-        prom = (var1+var2+var3)/3;
-
-        if (prom >= 7){
-            Toast.makeText(this, "Aprobado", Toast.LENGTH_SHORT).show();
-        }else{
-            Toast.makeText(this, "Reprobado", Toast.LENGTH_SHORT).show();
-        }
-
-
+    //Metodo de suma
+    public void sumar(View view){
+        String val1 = et1.getText().toString();
+        String val2 = et2.getText().toString();
+        int num1 = Integer.parseInt(val1);
+        int num2 = Integer.parseInt(val2);
+        int sum = num1 + num2;
+        String result = String.valueOf(sum);
+        tv1.setText(result);
     }
 
 
