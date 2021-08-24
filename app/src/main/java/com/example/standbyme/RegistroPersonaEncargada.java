155b0a0d280df16f38e17ad5562fe81d24145130
Herziox <herziox@gmail.com>
2021-08-24 17:36:37 -0500

    Desarrollo

---
+++ app/src/main/java/com/example/standbyme/RegistroPersonaEncargada.java
@@ -0,0 +1,14 @@
+package com.example.standbyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroPersonaEncargada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_persona_encargada);
+    }
+}
