7fda65e0e306c9706ed1246229c79bf5ea7fe5bf
Herziox <herziox@gmail.com>
2021-08-24 23:50:24 -0500

    diseño de la interfaz de login

--- app/src/main/java/com/example/standbyme/MainActivity.java
+++ app/src/main/java/com/example/standbyme/MainActivity.java
@@ -10,29 +10,14 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText et1, et2;
-    private TextView tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //La clase R es una clase de comunicacion entre la parte logica y grafica de la app
-        et1 = (EditText) findViewById(R.id.txt_num1);
-        et2 = (EditText) findViewById(R.id.txt_num2);
-        tv1 = (TextView) findViewById(R.id.txt_resultado);
     }
 
-    //Metodo de suma
-    public void sumar(View view){
-        String val1 = et1.getText().toString();
-        String val2 = et2.getText().toString();
-        int num1 = Integer.parseInt(val1);
-        int num2 = Integer.parseInt(val2);
-        int sum = num1 + num2;
-        String result = String.valueOf(sum);
-        tv1.setText(result);
-    }
+
 
 
 }
--- app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
+++ app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
@@ -3,12 +3,67 @@ package com.example.standbyme;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegistroAdultoMayor extends AppCompatActivity {
+
+    private EditText etxNombre, etxApellido, etxNumCel, etxObservaciones, etxFechaNac, etxContra, etxConContra;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_adulto_mayor);
+
+        //Captura de datos de la interfaz
+        etxNombre = (EditText)findViewById(R.id.txtNombreAM);
+        etxApellido = (EditText)findViewById(R.id.txtApellidoAM);
+        etxNumCel = (EditText)findViewById(R.id.txtNumCelAM);
+        etxObservaciones = (EditText)findViewById(R.id.txtObservacionesAM);
+        etxFechaNac = (EditText)findViewById(R.id.txtFechaNacAM);
+        etxContra = (EditText)findViewById(R.id.txtContraAM);
+        etxConContra = (EditText)findViewById(R.id.txtConContraAM);
+    }
+
+    public void Registrar(View view){
+        String nombreAM = etxNombre.getText().toString();
+        String apellidoAM = etxApellido.getText().toString();
+        String numCelAM = etxNumCel.getText().toString();
+        String observacionesAM = etxObservaciones.getText().toString();
+        String fechaNacAM = etxFechaNac.getText().toString();
+        String contraAM = etxConContra.getText().toString();
+        String conContraAM = etxConContra.getText().toString();
+        boolean registroLleno = true;
+
+
+        if (nombreAM.length() == 0){
+            registroLleno = false;
+        }
+        if (apellidoAM.length() == 0){
+            registroLleno = false;
+        }
+        if (numCelAM.length() == 0){
+            registroLleno = false;
+        }
+        if (fechaNacAM.length() == 0){
+            registroLleno = false;
+        }
+        if (contraAM.length() == 0){
+            registroLleno = false;
+        }
+        if (conContraAM.length() == 0){
+            registroLleno = false;
+        }
+        if (!conContraAM.equals(contraAM)){
+            Toast.makeText(this, "La contraseña no coincide", Toast.LENGTH_SHORT).show();
+            registroLleno = false;
+        }
+        if (registroLleno){
+            Toast.makeText(this, "Registro lleno", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(this, "Por favor llene correctamente los campos", Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
