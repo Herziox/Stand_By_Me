29001e0c485242be54f0bc53e5541e1fa65c4264
Herziox <herziox@gmail.com>
2021-08-24 22:46:57 -0500

    Interfaz de Adulto Mayor

---
+++ app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
@@ -0,0 +1,14 @@
+package com.example.standbyme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegistroAdultoMayor extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_adulto_mayor);
+    }
+}
