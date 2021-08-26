176ed0cc72d55ac9b062875bdcd79254089276b1
Herziox <herziox@gmail.com>
2021-08-26 12:14:24 -0500

    prueba texto

--- app/src/main/java/com/example/standbyme/SeleccionarUsuario.java
+++ app/src/main/java/com/example/standbyme/SeleccionarUsuario.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class SeleccionarUsuario extends AppCompatActivity {
 
@@ -12,6 +13,10 @@ public class SeleccionarUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seleccionar_usuario);
+
+        TextView txtTituloSelUsu = (TextView) findViewById(R.id.lblSeleccionarUsuario);
+
+
     }
 
     //Seleccion Persona Encargada
