69a08d6b0d536f2c4c5576a9169fa6bbd774dc6b
adrianrd7 <adrianlaje2@hotmail.com>
2021-08-26 23:29:51 -0500

    insertar Persona Encargada Firebase

--- app/src/main/java/com/example/standbyme/RegistroPersonaEncargada.java
+++ app/src/main/java/com/example/standbyme/RegistroPersonaEncargada.java
@@ -8,27 +8,47 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.standbyme.model.PersonaEncargada;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.UUID;
+
 public class RegistroPersonaEncargada extends AppCompatActivity {
-    private EditText etxNombre, etxApellido, etxNumCel, etxObservaciones, etxFechaNac, etxContra, etxConContra;
+    private EditText etxNombre, etxApellido, etxNumCel, etxObservaciones, etxCorreoElec, etxContra, etxConContra;
+
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_persona_encargada);
         //Captura de datos de la interfaz
-        etxNombre = (EditText) findViewById(R.id.txtNombre);
-        etxApellido = (EditText) findViewById(R.id.txtApellido);
-        etxNumCel = (EditText) findViewById(R.id.txtNumCel);
-        etxFechaNac = (EditText) findViewById(R.id.txtCorreoElectronicoPE);
-        etxContra = (EditText) findViewById(R.id.txtContra);
-        etxConContra = (EditText) findViewById(R.id.txtConContra);
+        etxNombre = (EditText) findViewById(R.id.txtNombrePE);
+        etxApellido = (EditText) findViewById(R.id.txtApellidoPE);
+        etxNumCel = (EditText) findViewById(R.id.txtNumCelPE);
+        etxCorreoElec = (EditText) findViewById(R.id.txtCorreoElectronicoPE);
+        etxContra = (EditText) findViewById(R.id.txtContraPE);
+        etxConContra = (EditText) findViewById(R.id.txtConContraPE);
+
+        inicializarFirebase();
+
+
+
+    }
+    public void inicializarFirebase(){
+        FirebaseApp.initializeApp(this);
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
 
     public void Registrar(View view) {
         String nombre = etxNombre.getText().toString();
         String apellido = etxApellido.getText().toString();
         String numCelAM = etxNumCel.getText().toString();
-        String fechaNac = etxFechaNac.getText().toString();
+        String correoElectronico = etxCorreoElec.getText().toString();
         String contra = etxConContra.getText().toString();
         String conContra = etxConContra.getText().toString();
         boolean registroLleno = true;
@@ -43,7 +63,7 @@ public class RegistroPersonaEncargada extends AppCompatActivity {
         if (numCelAM.length() == 0) {
             registroLleno = false;
         }
-        if (fechaNac.length() == 0) {
+        if (correoElectronico.length() == 0) {
             registroLleno = false;
         }
         if (contra.length() == 0) {
@@ -57,10 +77,20 @@ public class RegistroPersonaEncargada extends AppCompatActivity {
             registroLleno = false;
         }
         if (registroLleno) {
+
+            PersonaEncargada pe = new PersonaEncargada();
+            pe.setUid(UUID.randomUUID().toString());
+            pe.setNombre(nombre);
+            pe.setApellido(apellido);
+            pe.setNumeroCelular(numCelAM);
+            pe.setCorreoElectronico(correoElectronico);
+            pe.setContraseña(contra);
+            databaseReference.child("PersonaEncargada").child(pe.getUid()).setValue(pe);
             Toast.makeText(this, "Registro lleno", Toast.LENGTH_SHORT).show();
-            System.out.println("res: " + nombre + " " + apellido + " " + numCelAM  + " " + fechaNac + " " + contra + "\n");
+            System.out.println("res: " + nombre + " " + apellido + " " + numCelAM  + " " + correoElectronico + " " + contra + "\n");
             Intent siguiente = new Intent(this, RegistroAdultoMayor.class);
             startActivity(siguiente);
+
         } else {
             Toast.makeText(this, "Por favor llene correctamente los campos", Toast.LENGTH_SHORT).show();
         }
---
+++ app/src/main/java/com/example/standbyme/model/PersonaEncargada.java
@@ -0,0 +1,67 @@
+package com.example.standbyme.model;
+
+public class PersonaEncargada {
+
+    private String uid;
+    private String nombre;
+    private String apellido;
+    private String numeroCelular;
+    private String correoElectronico;
+    private String contraseña;
+
+    public PersonaEncargada() {
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNumeroCelular() {
+        return numeroCelular;
+    }
+
+    public void setNumeroCelular(String numeroCelular) {
+        this.numeroCelular = numeroCelular;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public String toString() {
+        return nombre;
+    }
+}
