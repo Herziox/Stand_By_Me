b895c74fa5ef13d0a391afdae69ee72dec3d8414
adrianrd7 <adrianlaje2@hotmail.com>
2021-08-28 18:10:05 -0500

    Agregado Menu Interfaz Adulto Mayor

--- app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
+++ app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
@@ -1,11 +1,15 @@
 package com.example.standbyme;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.standbyme.model.AdultoMayor;
 import com.google.firebase.database.DatabaseReference;
@@ -30,5 +34,30 @@ public class RegistroAdultoMayor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_adulto_mayor);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_adulto_mayor,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.icon_add:{
+                Toast.makeText(this, "Agregado", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.icon_save:{
+                Toast.makeText(this, "Guardado", Toast.LENGTH_SHORT).show();
+                break;
+            }
+            case R.id.icon_delete:{
+                Toast.makeText(this, "Eliminado", Toast.LENGTH_SHORT).show();
+                break;
+            }
+        }
+        return true;
     }
 }
