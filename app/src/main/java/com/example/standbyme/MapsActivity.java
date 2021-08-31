1ae641ed7bf40df6551625a4760ae02ff1036376
HenryTumailli <88125899+HenryTumailli@users.noreply.github.com>
2021-08-30 20:20:03 -0500

    Implementacion Google Maps

---
+++ app/src/main/java/com/example/standbyme/MapsActivity.java
@@ -0,0 +1,72 @@
+package com.example.standbyme;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.example.standbyme.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
+    private LocationManager ubicacion;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMapsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION
+            },1000);
+        }
+        ubicacion=(LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Location loc =ubicacion.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        mMap.setMyLocationEnabled(true);
+
+        // Add a marker in Sydney and move the camera
+        if(ubicacion!= null){
+            LatLng sydney = new LatLng(loc.getLatitude(), loc.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(sydney).title("Ubicación Actual"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+
+    }
+}
--- app/src/main/java/com/example/standbyme/RegistroResidencia.java
+++ app/src/main/java/com/example/standbyme/RegistroResidencia.java
@@ -1,15 +1,18 @@
 package com.example.standbyme;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistroResidencia extends AppCompatActivity {
 
     private EditText  etxMetros, etxLatitud, etxLongitud;
+    private Button btnMapa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +24,14 @@ public class RegistroResidencia extends AppCompatActivity {
         etxMetros = (EditText) findViewById(R.id.txtMetros);
         etxLatitud = (EditText) findViewById(R.id.txtLatitud);
         etxLongitud = (EditText) findViewById(R.id.txtLongitud);
+
+        btnMapa.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     public void Registrar(View view) {
