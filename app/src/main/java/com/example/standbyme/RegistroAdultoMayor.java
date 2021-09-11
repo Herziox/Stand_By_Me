5f7683c27be34011a2d63cc63713b1b673ce9d1d
adrianrd7 <adrianlaje2@hotmail.com>
2021-09-10 19:14:30 -0500

    Corregida interfaz Adulto Mayor

--- app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
+++ app/src/main/java/com/example/standbyme/RegistroAdultoMayor.java
@@ -3,6 +3,7 @@ package com.example.standbyme;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -31,7 +33,7 @@ public class RegistroAdultoMayor extends AppCompatActivity {
     private List<AdultoMayor> listAdultoMayor = new ArrayList<AdultoMayor>();
     ArrayAdapter<AdultoMayor> arrayAdapterAbuelitos;
 
-    private EditText nomAM, appAM, cedulaAM, numtelfAM,  fechaNacimientoAM,psswordAM,rePpasswordAM, observacionesAM, rangoAM, latitudaAM, longitudAM;
+    private EditText nomAM, appAM, cedulaAM, numtelfAM,  fechaNacimientoAM,psswordAM,rePpasswordAM, observacionesAM, rangoAM, latitudaAM, longitudAM ;
     private ListView listV_AdultoMayor;
 
     private FirebaseAuth mFirebaseAuth;
@@ -63,6 +65,17 @@ public class RegistroAdultoMayor extends AppCompatActivity {
 
         listarDatos();
 
+        fechaNacimientoAM.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+                    case R.id.textFieldFechaDeNacimientoAM:
+                        showDatePickerDialog();
+                        break;
+                }
+            }
+        });
+
         listV_AdultoMayor.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -82,6 +95,20 @@ public class RegistroAdultoMayor extends AppCompatActivity {
         });
 
 
+    }
+
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                final String selectedDate = twoDigits(day) + "/" + twoDigits(month+1) + "/" + year;
+                fechaNacimientoAM.setText(selectedDate);
+            }
+        });
+        newFragment.show(getSupportFragmentManager(), "datePicker");
+    }
+    private String twoDigits(int n) {
+        return (n<=9) ? ("0"+n) : String.valueOf(n);
     }
 
     private void listarDatos() {
@@ -140,10 +167,10 @@ public class RegistroAdultoMayor extends AppCompatActivity {
 
                 if (nombre.isEmpty() || apellido.isEmpty() || numCedula.isEmpty() || numTelefono.isEmpty()
                         || fechaNacimiento.isEmpty() || password.isEmpty() || rePassword.isEmpty()){
-                    validacion();
+                    validacionVacios();
                 }
                 else{
-                    if (password.equals(rePassword)){
+                    if (validacion()){
                         String idPE = mFirebaseAuth.getCurrentUser().getUid();
                         AdultoMayor am = new AdultoMayor();
                         am.setUid(numCedula);
@@ -161,8 +188,6 @@ public class RegistroAdultoMayor extends AppCompatActivity {
                         databaseReference.child("AdultoMayor1").child(am.getUid()).setValue(am);
                         Toast.makeText(RegistroAdultoMayor.this, "Agregado", Toast.LENGTH_SHORT).show();
                         limpiarCajas();
-                    }else{
-                        rePpasswordAM.setError("Las contraseñas no coinciden");
                     }
                 }
 
@@ -228,7 +253,7 @@ public class RegistroAdultoMayor extends AppCompatActivity {
         startActivity(siguiente);
     }
 
-    private void validacion() {
+    private void validacionVacios() {
         String nombre = nomAM.getText().toString();
         String apellido = appAM.getText().toString();
         String numCedula = cedulaAM.getText().toString();
@@ -258,7 +283,64 @@ public class RegistroAdultoMayor extends AppCompatActivity {
         if (rePassword.isEmpty()){
             rePpasswordAM.setError("Requerido");
         }
+    }
+    private boolean validacion(){
 
+        String nombre = nomAM.getText().toString();
+        String apellido = appAM.getText().toString();
+        String numCedula = cedulaAM.getText().toString();
+        String numTelefono = numtelfAM.getText().toString();
+        String fechaNacimiento = fechaNacimientoAM.getText().toString();
+        String password = psswordAM.getText().toString();
+        String rePassword = rePpasswordAM.getText().toString();
+        if (isEcuadorianDocumentValid(numCedula) == false){
+            cedulaAM.setError("El número de cédula es incorrecto");
+            return false;
+        }
+        if (numTelefono.length() < 10 ){
+            numtelfAM.setError("El número celular es incorrecto");
+            return false;
+        }
+        if (password.length() >= 6){
+            if (!password.equals(rePassword)){
+                rePpasswordAM.setError("La contraseña no coincide");
+                return false;
+            }
+        }else{
+            psswordAM.setError("La contraseña debe ser mayor de 6 digitos");
+            return false;
+        }
+        return true;
+    }
+    private boolean isEcuadorianDocumentValid(String document) {
+        byte sum = 0;
+        try {
+            if (document.trim().length() != 10)
+                return false;
+            String[] data = document.split("");
+            byte verifier = Byte.parseByte(data[0] + data[1]);
+            if (verifier < 1 || verifier > 24)
+                return false;
+            byte[] digits = new byte[data.length];
+            for (byte i = 0; i < digits.length; i++)
+                digits[i] = Byte.parseByte(data[i]);
+            if (digits[2] > 6)
+                return false;
+            for (byte i = 0; i < digits.length - 1; i++) {
+                if (i % 2 == 0) {
+                    verifier = (byte) (digits[i] * 2);
+                    if (verifier > 9)
+                        verifier = (byte) (verifier - 9);
+                } else
+                    verifier = (byte) (digits[i] * 1);
+                sum = (byte) (sum + verifier);
+            }
+            if ((sum - (sum % 10) + 10 - sum) == digits[9])
+                return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 
 }
