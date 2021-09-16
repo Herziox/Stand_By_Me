923b406c13f85052c7f785d1016dbef8b9664195
Kevin Pallo <65378375+KevinPallo@users.noreply.github.com>
2021-09-15 19:07:29 -0500

    CommitUbicaciónActual
    Al momento de implementar la parte de ubicación actual, tuve problemas con la busqueda por texto.
    Trataré de arreglar ese problema.

--- app/src/main/java/com/example/standbyme/registro_Localizacion.java
+++ app/src/main/java/com/example/standbyme/registro_Localizacion.java
@@ -4,24 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
 import com.google.android.gms.location.GeofencingRequest;
@@ -38,14 +43,19 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.IOException;
 import java.util.List;
 
-public class registro_Localizacion extends AppCompatActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener {
+public class registro_Localizacion extends FragmentActivity implements OnMapReadyCallback{
     private static final String TAG = "registro_Localizacion";
     private GoogleMap mMap;
+    SupportMapFragment mapFragment;
+    FusedLocationProviderClient client; // ubicación actual 15-09-2021
+    SearchView searchView;
     private LocationManager ubicacion;
     private GeofencingClient geofencingClient;
     private GeoferenceHelper geoferenceHelper;
@@ -58,13 +68,112 @@ public class registro_Localizacion extends AppCompatActivity implements OnMapRea
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_localizacion);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+        searchView = findViewById(R.id.sv_location);
+        //Inicializar mapFragment
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        //Inicialiar fused Location
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        //Revisar permisos
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        }else{
+            //Cuando el permiso denegado
+            ActivityCompat.requestPermissions(registro_Localizacion.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},44);
+        }
+
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                //mMap.clear();
+                String location = searchView.getQuery().toString();
+                List<Address> addressList = null;
+                if (location != null || !location.equals("")) {
+                    Geocoder geocoder = new Geocoder(registro_Localizacion.this);
+                    try {
+                        addressList = geocoder.getFromLocationName(location, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    Address address = addressList.get(0);
+                    LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+
         mapFragment.getMapAsync(this);
         geofencingClient = LocationServices.getGeofencingClient(this);
         geoferenceHelper = new GeoferenceHelper(this);
     }
 
+    //Método Current Location 15-09-2021
+    public void getCurrentLocation() {
+        // Inicializar el task location
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        return;
+        }
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                //Cuando es satisfactorio
+                if(location != null){
+                    mapFragment.getMapAsync(new OnMapReadyCallback() {
+                        @Override
+                        public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+                            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                            MarkerOptions options = new MarkerOptions().position(latLng)
+                                    .title("Mi ubicación actual");
+                            googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10));
+                            googleMap.addMarker(options);
+
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 44) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getCurrentLocation();
+            } else {
+
+            }
+        }
+
+
+
+        /*if (requestCode == BACKGROUND_LOCATION_ACCES_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "You can add geofences...",Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Background location access is necessary for geofences" +
+                        "to trigger", Toast.LENGTH_SHORT).show();
+            }
+        }*/
+    }
+
+    @Override
+    public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
+
+    }
+/*
     private void enableUserLocation() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) ==
                 PackageManager.PERMISSION_GRANTED) {
@@ -109,7 +218,7 @@ public class registro_Localizacion extends AppCompatActivity implements OnMapRea
 
     @Override
     public void onMapReady(
-            GoogleMap googleMap) {
+        GoogleMap googleMap) {
         mMap = googleMap;
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
         mMap.getUiSettings().setZoomControlsEnabled(true);
@@ -193,5 +302,5 @@ public class registro_Localizacion extends AppCompatActivity implements OnMapRea
         circleOptions.strokeWidth(4);
         mMap.addCircle(circleOptions);
     }
-
+*/
 }
