04f42ad3f45512da83afe1dbf227652b6b5d23d5
adrianrd7 <adrianlaje2@hotmail.com>
2021-09-19 01:58:59 -0500

    Agregado buscador

--- app/src/main/java/com/example/standbyme/registro_Localizacion.java
+++ app/src/main/java/com/example/standbyme/registro_Localizacion.java
@@ -6,48 +6,76 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
+
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle;
-import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.jetbrains.annotations.NotNull;
 
-public class registro_Localizacion extends AppCompatActivity implements OnMapReadyCallback{
-    private static final int LOCATION_REQUEST_CODE = 1;
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class registro_Localizacion extends AppCompatActivity implements OnMapReadyCallback,
+        LocationListener,GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
+
+private static final int LOCATION_REQUEST_CODE = 1;
     public final static String EXTRA_LATITUD = "TUPAQUETE.petmotion.LATITUD";
     public final static String EXTRA_LONGITUD = "TUPAQUETE.petmotion.LONGITUD";
-    private Marker markerPosition;
     private Button confirmarButton;
     private GoogleMap mMap;
-
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    GoogleApiClient mGoogleApiClient;
+    LocationRequest mLocationRequest;
+    private SupportMapFragment mapFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_localizacion);
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
-    }
 
+        confirmarButton = (Button)findViewById(R.id.btnConfirmar_registro_localizacion);
+        mapaDisponible();
+    }
+    /*Método que permitirá comprobar si el control asignado a la variable de tipo MapFragment se ha definido
+    en el layout de la Activity.*/
+    private void mapaDisponible()
+    {
+        if(mapFragment == null)
+        {
+            mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(registro_Localizacion.this);
+        }
+
+        if(mapFragment != null)
+        {
+            Toast.makeText(this, "Mapa de Google disponible", Toast.LENGTH_SHORT).show();
+        }
+    }
 
     @Override
     public void onMapReady(@NonNull @NotNull GoogleMap googleMap) {
@@ -72,39 +100,50 @@ public class registro_Localizacion extends AppCompatActivity implements OnMapRea
 
         mMap.getUiSettings().setZoomControlsEnabled(true);
         // Marcadores
-        LatLng position = new LatLng(-0.210146, -78.490165);
-        markerPosition = googleMap.addMarker(new MarkerOptions()
-                .position(position)
-                .title("Posición")
-                .draggable(true)
-        );
+        LatLng posicionMarcador = new LatLng(-0.210146, -78.490165);;
+        MarkerOptions markerOptions =
+                new MarkerOptions()
+                        .position(posicionMarcador)
+                        .title("Posicion")
+                        .draggable(true);
+
+        mCurrLocationMarker = googleMap.addMarker(markerOptions);
+
         // Cámara
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(position));
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(posicionMarcador));
         // Eventos
-        confirmarButton = (Button)findViewById(R.id.btnConfirmar_registro_localizacion);
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(@NonNull Marker marker) {
+                Toast.makeText(registro_Localizacion.this, "START", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onMarkerDrag(@NonNull Marker marker) {
+                if (marker.equals(marker)) {
+                    String newTitle = String.format(Locale.getDefault(),
+                            getString(R.string.marker_detail_latlng),
+                            marker.getPosition().latitude,
+                            marker.getPosition().longitude);
+                    setTitle(newTitle);
+                }
+            }
+            @Override
+            public void onMarkerDragEnd(@NonNull Marker marker) {
+                    if (marker.equals(marker)) {
+                        Toast.makeText(registro_Localizacion.this, "END", Toast.LENGTH_SHORT).show();
+                    }
+            }
+        });
         confirmarButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent( registro_Localizacion.this, RegistroAdultoMayor.class);
-                intent.putExtra(EXTRA_LATITUD, markerPosition.getPosition().latitude);
-                intent.putExtra(EXTRA_LONGITUD, markerPosition.getPosition().longitude);
+                intent.putExtra(EXTRA_LATITUD, mCurrLocationMarker.getPosition().latitude);
+                intent.putExtra(EXTRA_LONGITUD, mCurrLocationMarker.getPosition().longitude);
                 startActivity(intent);
             }
         });
-        //Añadir circulo
-        LatLng center = new LatLng(markerPosition.getPosition().latitude, markerPosition.getPosition().longitude);
-        int radius = 40;
-        CircleOptions circleOptions = new CircleOptions()
-                .center(center)
-                .radius(radius)
-                .strokeColor(Color.parseColor("#0D47A1"))
-                .strokeWidth(4)
-                .fillColor(Color.argb(32, 33, 150, 243));
-        // Añadir círculo
-        Circle circle = mMap.addCircle(circleOptions);
-        //(Opcional) Actualiza el objetivo de la cámara:
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 17));
-
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(posicionMarcador, 17));
 
     }
 
@@ -133,5 +172,87 @@ public class registro_Localizacion extends AppCompatActivity implements OnMapRea
 
         }
     }
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(1000);
+        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+        mLastLocation = location;
+        if (mCurrLocationMarker != null) {
+            mCurrLocationMarker.remove();
+        }
+        //Place current location marker
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.position(latLng);
+        markerOptions.title("Current Position");
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+        mCurrLocationMarker = mMap.addMarker(markerOptions);
+
+        //move map camera
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(11));
+
+        //stop location updates
+        if (mGoogleApiClient != null) {
+            LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    public void searchLocation(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.editText);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            //mMap.addMarker(new MarkerOptions().position(latLng).title(location));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            Toast.makeText(getApplicationContext(),address.getLatitude()+" "+address.getLongitude(),Toast.LENGTH_LONG).show();
+            mCurrLocationMarker.setPosition(latLng);
+        }
+    }
 
 }
+
+
